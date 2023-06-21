90b1b748fbf47172e48647362441f8ceb2ab2b01
NATHAN JOSUÉ SILVA ANTUNES <nathan_j_antunes@estudante.sesisenai.org.br>
2023-06-21 16:54:53 -0300

    Commit Inicial

---
+++ logica/script.js
@@ -0,0 +1,9 @@
+let nomeDoAluno
+let idadeDoAluno
+nomeDoAluno = prompt('Qual é o seu nome')
+idadeDoAluno = prompt("Qual é a sua idade?")
+idadeDoAluno = Number (idadeDoAluno)
+
+idadeDoAluno = idadeDoAluno + 1
+
+alert("O mais Gato é o " + nomeDoAluno + "\n Idade: " + idadeDoAluno)
---
+++ logica/script313.js
@@ -0,0 +1,48 @@
+//Questão 5
+// let n1, antecessor
+
+// n1= Number(prompt("Digite um número"))
+
+// antecessor= n1 - 1
+
+// alert("O antecessor do número é: "+ antecessor)
+
+//Questão 7
+
+// let Anos, Meses, Dias, Dias_vividos
+
+// Anos= Number(prompt("Quantos anos você tem? "))
+// Meses= Number(prompt("...e quantos meses? "))
+// Dias= Number(prompt("...e quantos dias? "))
+
+// Dias_vividos= Anos * 365 + Meses * 30 + Dias
+
+// alert("O total de dias vividos é: " + Dias_vividos)
+
+//Questão 8
+
+// let NE, Brancos, Nulos, Válidos, PB, PN, PV
+
+// //NE= Number(prompt("Quantas pessoas votaram? "))
+// Brancos= Number(prompt("Quantos votos Brancos? "))
+// Nulos= Number(prompt("quantos votos anulados? "))
+// Válidos= Number(prompt("Quantos votos válidos? "))
+
+// NE= Brancos + Nulos+ Válidos
+
+// PB= Brancos/NE * 100
+// PN= Nulos/NE * 100
+// PV= Válidos/ NE * 100
+
+// alert("E o percentual foi: " + "\nBrancos: " + PB +"%" + "\nNulos: " + PN + "%" + "\nVálidos: " + PV + "%") 
+
+// Questão 9
+
+// let SM, PR, SN
+
+// SM= Number (prompt("Qual o seu salário mensal? "))
+// PR= Number (prompt("Qual o percentual do reajuste? "))
+
+// SN= SM+(SM* PR) /100
+
+// alert(" Seu novo salário é: R$" + SN)
