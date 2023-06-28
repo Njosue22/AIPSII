4a1977ceee1f99b2bd30225c27073d5a236b08a6
Njosue22 <137323183+Njosue22@users.noreply.github.com>
2023-06-28 17:18:46 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,30 @@
+function converte(){
+    var temperatura = Number(document.getElementById("inicial").value)
+    var resultado
+    if(document.getElementById("cf").checked){
+        resultado = (temperatura * 9/5) + 32
+    }
+    else if (document.getElementById("ck").checked){
+        resultado = temperatura + 273.15
+
+    }
+    else if (document.getElementById("fc").checked){
+        resultado = ( temperatura - 32) * 5/9 
+    }
+    else if (document.getElementById("fk").checked){
+       resultado = ( temperatura - 32) * 5/9 + 273.15
+    }
+    else if (document.getElementById("kc").checked){
+        resultado = temperatura - 273.15
+    }
+    else if (document.getElementById("kf").checked){
+        resultado = (temperatura - 273.15) * 9/5 + 32
+    }
+    else {
+         resultado = "Por favor, selecione algo"
+    }
+
+         document.getElementById("resultado").innerHTML = resultado
+
+    }
+    
